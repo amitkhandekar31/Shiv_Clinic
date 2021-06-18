525402576d9f3eb8895f1dbd858b5f1a6edfcacc
amitkhandekar31 <avkhandekar31.com>
2021-06-18 14:23:00 +0530

    added new route of email

--- src/Project/Contact/Contact.js
+++ src/Project/Contact/Contact.js
@@ -114,7 +114,8 @@ const clientmobileRegex = RegExp(/^[0-9][0-9]{9}$/);
 	      };
 	      if(formValid(this.state.formerrors)){
 		        axios
-		        .post('/api/form',formValues1)
+		        // .post('/api/form',formValues1)
+		        .post('/.netlify/functions/server/api/form',formValues1)
 		        .then((res)=>{
 		                   if(res.status === 200){
 		                    swal("Thank you for contacting us. We will get back to you shortly.")
@@ -153,7 +154,8 @@ const clientmobileRegex = RegExp(/^[0-9][0-9]{9}$/);
       // console.log("notification",formValues2); 
       if(formValid(this.state.formerrors)){
         axios
-        .post('/api/form',formValues2)
+        // .post('/api/form',formValues2)
+        .post('/.netlify/functions/server/api/form',formValues2)
         .then((res)=>{
                   if(res.status === 200){
                     console.log("Mail Sent TO ADMIN successfully!")
